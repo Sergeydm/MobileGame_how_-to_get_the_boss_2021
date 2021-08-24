5b7ee343ede2e5caacca8d5d9cf52d306b09c352
Sergeydm <32673289+Sergeydm@users.noreply.github.com>
2021-08-24 13:57:53 +0500

    Локальное обновление

---
+++ RTS_2019/Assets/SensorToolkit/Examples/Stealth/resources/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SensorToolkit.Example
+{
+    [RequireComponent(typeof(CharacterControls))]
+    public class PlayerInput : MonoBehaviour
+    {
+        public Sensor InteractionRange;
+
+        CharacterControls cc;
+        
+        void Start()
+        {
+            cc = GetComponent<CharacterControls>();
+        }
+
+        void Update()
+        {
+
+
+
+            // Project mouse position onto worlds plane at y=0
+
+            var camPosition = Camera.main.transform.position;
+            var camForward = Camera.main.transform.forward;
+            var camDepthToGroundPlane = -camPosition.y / camForward.y;
+
+            // Pickup the pickup if its in range
+            var pickup = InteractionRange.GetNearestByComponent<Holdable>();
+            if (pickup != null && !pickup.IsHeld)
+            {
+                cc.PickUp(pickup);
+            }
+        }
+    }
+}
